3ab76bf7edb47d92dc5d2899e4ede998a0f89881
niceyoo0221 <yoo020221@naver.com>
2022-07-18 13:34:19 +0900

    로그인 구현

--- MainApp/views.py
+++ MainApp/views.py
@@ -5,19 +5,18 @@ def register(request):
     return render(request, 'register.html')
 
 def login(request):
+    bad_login = False
     if (request.method == 'POST'):
         user_id = request.POST['username']
         password = request.POST['password']
         user = auth.authenticate(request, username=user_id, password=password)
-        bad_login = 0
         if user is not None:
             auth.login(request, user)
             return redirect('home')
         else:
-            bad_login = 1
-            return redirect('home', {'bad_login':bad_login})
-            
-    return render(request, 'login.html')
+            bad_login = True
+            return render(request, 'login.html', {'bad_login':bad_login})
+    return render(request, 'login.html', {'bad_login':bad_login})
 
 def home(request):
     return render(request, 'index.html')
