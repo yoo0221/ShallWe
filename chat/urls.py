d9c614db49dfefb272acf4e2ff8638d46e80d59b
niceyoo0221 <yoo020221@naver.com>
2022-08-16 17:53:32 +0900

    url 오류 수정

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,8 @@
 # chat/urls.py
 from django.urls import path
-
 from chat import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='chat'),
     path('<str:room_name>/', views.room, name='room'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
-
-# Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'chat_index.html')
 
 def room(request, room_name):
-    return render(request, 'room.html', {
+    return render(request, 'chat_room.html', {
         'room_name': room_name
     })
