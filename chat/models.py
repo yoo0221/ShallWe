5553bf684fc8c3f53a4a76ea36f37be0359b02e3
niceyoo0221 <yoo020221@naver.com>
2022-08-18 23:38:35 +0900

    chatting 테마 선택 중간 저장

--- chat/models.py
+++ chat/models.py
@@ -11,4 +11,19 @@ class Message(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user', db_column='user', null=True, max_length=100)
     created_time = models.DateTimeField(auto_now_add=True)
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='messages', db_column='room', null=True)
+    sort = models.CharField(max_length=30, null=True)
+
+class Thema(models.Model):
+    sort = models.CharField(max_length=10, null=True)
+    number = models.PositiveIntegerField()
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='thema1', db_column='user1')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='thema2', db_column='user2')
+
+class ThemaMessage(Message):
+    thema_sort = models.CharField(max_length=10, null=True)
+    number = models.PositiveIntegerField()
+    confirmed = models.BooleanField(default=False)
+
+
+
 
