5b21fc080ac29763f5643809f1943d28a1d31a0c
niceyoo0221 <yoo020221@naver.com>
2022-08-19 22:48:07 +0900

    chat room - thema 선택 및 제안 구현 완료

--- chat/models.py
+++ chat/models.py
@@ -12,18 +12,16 @@ class Message(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='messages', db_column='room', null=True)
     sort = models.CharField(max_length=30, null=True)
+    thema_sort = models.CharField(max_length=10, null=True, blank=True)
+    thema_num = models.IntegerField(null=True, blank=True)
+    thema_confirmed = models.BooleanField(null=True, blank=True)
+    promise_place = models.CharField(max_length=100, null=True, blank=True)
+    promise_address = models.CharField(max_length=100, null=True, blank=True)
+    promise_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    promise_confirmed = models.BooleanField(null=True, blank=True)
 
 class Thema(models.Model):
     sort = models.CharField(max_length=10, null=True)
     number = models.PositiveIntegerField()
     user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='thema1', db_column='user1')
     user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='thema2', db_column='user2')
-
-class ThemaMessage(Message):
-    thema_sort = models.CharField(max_length=10, null=True)
-    number = models.PositiveIntegerField()
-    confirmed = models.BooleanField(default=False)
-
-
-
-
